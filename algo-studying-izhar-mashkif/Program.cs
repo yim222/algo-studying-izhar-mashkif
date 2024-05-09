d106c103ca0d4ec7a60aceb15a12ae9f390bd5d6
yim222 <yimprogramming@gmail.com>
2024-05-09 20:11:33 +0300

    Saving but not running

--- algo-studying-izhar-mashkif/Program.cs
+++ algo-studying-izhar-mashkif/Program.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using algo_studying_izhar_mashkif.common;
 namespace algo_studying_izhar_mashkif
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Israel");
+            ProcessManager.RunProcess();
         }
     }
 }
---
+++ algo-studying-izhar-mashkif/common/ProcessManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algo_studying_izhar_mashkif.common
+{
+    public class ProcessManager
+    {
+
+        public static void RunProcess()
+        {
+            Console.WriteLine("Choose one option:\n"
+                +"1 - World | 2 - Israel | 0 - Exit" );
+        }
+    }
+}
